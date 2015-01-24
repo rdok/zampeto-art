91a4fc8939d652e955f47fb3ede29f79bc0715f9
Rizart Dokollari <r.dokollari@gmail.com>
2015-01-24 17:29:41 +0200

    Fix 'EIKONES' menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
 //Add Hover effect to menus
 jQuery('ul.nav li.dropdown').hover(function () {
-    jQuery(this).find('.dropdown-menu').stop(true, true).show();
+    //jQuery(this).find('.dropdown-menu').stop(true, true).show();
 }, function () {
-    jQuery(this).find('.dropdown-menu').stop(true, true).hide();
+    //jQuery(this).find('.dropdown-menu').stop(true, true).hide();
 });
 
 (function () {
-    $("li.dropdown a").click(function (e) {
-        $(this).next('ul.dropdown-menu').css("display", "block");
-        e.stopPropagation();
-    });
+    //$("li.dropdown a").click(function (e) {
+    //    $(this).next('ul.dropdown-menu').css("display", "block");
+    //    e.stopPropagation();
+    //});
 });
