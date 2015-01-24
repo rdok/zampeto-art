178183cc46f022e747028611985b7dc24ebac859
Rizart Dokollari <r.dokollari@gmail.com>
2015-01-24 18:10:14 +0200

    Use CWSpear/bootstrap-hover-dropdown for hover dropdown toggle.

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,1 @@
-//Add Hover effect to menus
-jQuery('ul.nav li.dropdown').hover(function () {
-    //jQuery(this).find('.dropdown-menu').stop(true, true).show();
-}, function () {
-    //jQuery(this).find('.dropdown-menu').stop(true, true).hide();
-});
 
-(function () {
-    //$("li.dropdown a").click(function (e) {
-    //    $(this).next('ul.dropdown-menu').css("display", "block");
-    //    e.stopPropagation();
-    //});
-});
