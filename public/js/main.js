5474110713f039c3395a107cca816fa88b9783d0
Rizart Dokollari <r.dokollari@gmail.com>
2015-01-27 23:27:49 +0200

    Prepare angularjs.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,44 @@
+/**
+ *
+ * @author Rizart Dokollari
+ * @version 1/27/2015
+ */
+(function ()
+{
 
+	var app = angular.module('zampetoArtApp', ['ngRoute']);
+
+	/**
+	 * Configure routes
+	 */
+	app.config(function ($routeProvider, $locationProvider)
+	{
+		$routeProvider
+			// Home
+			.when("/", {
+				templateUrl: "templates/home.html",
+				controller : "PageController",
+				activeTab  : '/'
+			})
+			.when("/ksylo",
+			{
+				templateUrl: "templates/ksylo.html",
+				controller : "PageController",
+				activeTab  : '#ksylo'
+			}) // else 404
+			.otherwise({
+				templateUrl: "templates/home.html",
+				controller : "PageController",
+				activeTab  : '/'
+			});
+
+		// use the HTML5 History API
+		$locationProvider.html5Mode(true);
+	});
+
+	app.controller('PageController', function ($scope, $route)
+	{
+		// Expose $route to controller
+		$scope.$route = $route;
+	});
+})();;
