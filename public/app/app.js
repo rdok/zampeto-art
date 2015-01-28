0cb4d7c7010f99da1930d51d432448fde2536295
Rizart Dokollari <r.dokollari@gmail.com>
2015-01-28 19:27:33 +0200

    Complete angularjs masonry feature

--- public/app/app.js
+++ public/app/app.js
@@ -10,12 +10,54 @@
 		{
 			title      : 'Γαλέρα',
 			description: 'Τέμπερα σε πάτο βαρελιού, διακόσμιση χρυσό 24Κ',
-			url       : '/lib/img/pages/ksylo/gallery-full-1.png',
+			url        : '/lib/img/pages/ksylo/gallery-full-1.jpeg',
 			thumb      : '/lib/img/pages/ksylo/gallery-thumb-1.png'
-		}
+		},
+		{
+			title      : 'Πολεμικό Καράβι',
+			description: 'Τέμπερα',
+			url        : '/lib/img/pages/ksylo/gallery-full-2.jpeg',
+			thumb      : '/lib/img/pages/ksylo/gallery-thumb-2.png'
+		},
+		{
+			title      : 'Γαλέρα',
+			description: 'Τέμπερα σε πάτο βαρελιού, διακόσμιση χρυσό 24Κ',
+			url        : '/lib/img/pages/ksylo/gallery-full-3.jpeg',
+			thumb      : '/lib/img/pages/ksylo/gallery-thumb-3.png'
+		},
+		{
+			title      : 'Τέμπερα',
+			description: 'Υδραίικο πολεμικό Καράβι',
+			url        : '/lib/img/pages/ksylo/gallery-full-4.jpeg',
+			thumb      : '/lib/img/pages/ksylo/gallery-thumb-4.png'
+		},
+		{
+			title      : 'Πίσω Λιβάδι Πάρου',
+			description: 'Τέμπερα σε πάτο βαρελιού',
+			url        : '/lib/img/pages/ksylo/gallery-full-5.jpeg',
+			thumb      : '/lib/img/pages/ksylo/gallery-thumb-5.png'
+		},
+		{
+			title      : 'Φορτηγό Πλοίο',
+			description: 'Τέμπερα',
+			url        : '/lib/img/pages/ksylo/gallery-full-6.jpeg',
+			thumb      : '/lib/img/pages/ksylo/gallery-thumb-6.jpeg'
+		},
+		{
+			title      : 'Ποταμόπλοιο',
+			description: 'Τέμπερα σε πάτο βαρελιού',
+			url        : '/lib/img/pages/ksylo/gallery-full-7.jpeg',
+			thumb      : '/lib/img/pages/ksylo/gallery-thumb-7.png'
+		},
+		{
+			title      : 'Tο Πλοίο η Ελλάς',
+			description: 'Τέμπερα',
+			url        : '/lib/img/pages/ksylo/gallery-full-8.jpeg',
+			thumb      : '/lib/img/pages/ksylo/gallery-thumb-8.jpeg'
+		},
 	];
 
-	var app = angular.module('zampetoArtApp', ['ngRoute', 'bootstrapLightbox']);
+	var app = angular.module('zampetoArtApp', ['ngRoute', 'bootstrapLightbox', 'wu.masonry']);
 
 	/**
 	 * Configure routes
