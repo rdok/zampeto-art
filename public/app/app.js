c00e6150a40de4222248d03ef5a3b1551e2cc804
Rizart Dokollari <r.dokollari@gmail.com>
2015-02-04 17:42:56 +0200

    Update line for active & remove lines from subcategory eikones.

--- public/app/app.js
+++ public/app/app.js
@@ -5,84 +5,83 @@
  */
 (function () {
 
-    var app = angular.module('zampetoArtApp', ['ngRoute', 'ngAnimate', 'wu.masonry', 'bootstrapLightbox', 'page-common', 'page-woods', 'page-contact']);
+	var app = angular.module('zampetoArtApp', ['ngRoute', 'ngAnimate', 'wu.masonry', 'bootstrapLightbox', 'page-common', 'page-woods', 'page-contact']);
 
-    /**
-     * Configure routes
-     */
-    app.config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-            // Home
-            .when("/", {
-                templateUrl: "/pages/home.html",
-                controller: "PageController",
-                activeTab: '/'
-            })
-            .when("/wood",
-            {
-                templateUrl: "/pages/wood.html",
-                controller: "WoodPaintingController",
-                activeTab: '#wood'
-            })
-            .when("/contact",
-            {
-                templateUrl: "/pages/contact.html",
-                controller: "ContactPaintingController",
-                activeTab: '#contact'
-            }) // else 404
+	/**
+	 * Configure routes
+	 */
+	app.config(function ($routeProvider, $locationProvider) {
+		$routeProvider
+			// Home
+			.when("/", {
+				templateUrl: "/pages/home.html",
+				controller : "PageController",
+				activeTab  : '/'
+			})
+			.when("/wood",
+			{
+				templateUrl: "/pages/wood.html",
+				controller : "WoodPaintingController",
+				activeTab  : '#wood'
+			})
+			.when("/contact",
+			{
+				templateUrl: "/pages/contact.html",
+				controller : "ContactPaintingController",
+				activeTab  : '#contact'
+			}) // else 404
 
-            .otherwise("/404", {
-                templateUrl: "/partials/home.html",
-                controller: "PageController",
-                activeTab: '/'
-            });
+			.otherwise("/404", {
+				templateUrl: "/partials/home.html",
+				controller : "PageController",
+				activeTab  : '/'
+			});
 
-        // use the HTML5 History API
-        $locationProvider.html5Mode(true);
-    });
+		// use the HTML5 History API
+		$locationProvider.html5Mode(true);
+	});
 
-    app.controller('PageController', function ($http, $scope, $route, $sce) {
-        // Expose $route to controller
-        $scope.$route = $route;
+	app.controller('PageController', function ($http, $scope, $route, $sce) {
+		// Expose $route to controller
+		$scope.$route = $route;
 
-        var currentCtrl = this;
+		var currentCtrl = this;
 
-        $http.get('/app/common/services/models/lang-gr.json')
-            .success(function (data) {
-                currentCtrl.lang = data;
-            });
-        this.selectLanguage = function (lang) {
-            if (lang === "EN") {
-                $http.get('/app/common/services/models/lang-en.json')
-                    .success(function (data) {
-                        currentCtrl.lang = data;
-                    });
-                return;
-            }
+		$http.get('/app/common/services/models/lang-gr.json')
+			.success(function (data) {
+				currentCtrl.lang = data;
+			});
+		this.selectLanguage = function (lang) {
+			if (lang === "EN") {
+				$http.get('/app/common/services/models/lang-en.json')
+					.success(function (data) {
+						currentCtrl.lang = data;
+					});
+				return;
+			}
 
-            $http.get('/app/common/services/models/lang-gr.json')
-                .success(function (data) {
-                    currentCtrl.lang = data;
-                });
-        };
+			$http.get('/app/common/services/models/lang-gr.json')
+				.success(function (data) {
+					currentCtrl.lang = data;
+				});
+		};
 
-
-        /**
-         * http://stackoverflow.com/a/20710548/2790481
-         * @param htmlCode
-         * @returns {*}
-         */
-        currentCtrl.toTrusted = function (htmlCode) {
-            return $sce.trustAsHtml(htmlCode);
-        }
-    });
-    app.directive('dropdownMenu', function () {
-        return {
-            link: function (scope, elem) {
-                elem.dropdownHover();
-                elem.dropdown();
-            }
-        };
-    });
+		/**
+		 * http://stackoverflow.com/a/20710548/2790481
+		 * @param htmlCode
+		 * @returns {*}
+		 */
+		currentCtrl.toTrusted = function (htmlCode) {
+			return $sce.trustAsHtml(htmlCode);
+		}
+	});
+	app.directive('dropdownMenu', function () {
+		return {
+			link: function (scope, elem) {
+				elem.dropdownHover();
+				//elem.dropdown();
+			}
+		};
+	});
 
 })();
