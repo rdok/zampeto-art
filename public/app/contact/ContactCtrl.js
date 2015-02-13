98a422940ed1f5f71dea1ae749c282ea609b68b4
Rizart Dokollari <r.dokollari@gmail.com>
2015-02-13 17:21:40 +0200

    Add mailgun service & mail feature sent.

--- public/app/contact/ContactCtrl.js
+++ public/app/contact/ContactCtrl.js
@@ -4,27 +4,26 @@
  * @version 1/27/2015
  */
 (function () {
-    var app = angular.module('contactCtrl', []);
+	var app = angular.module('contactCtrl', []);
 
+	app.controller('ContactController', function ($route, $http) {
+		this.contact = {};
 
-    app.controller('ContactController', function ($route, $http) {
-        this.reservation = {};
+		this.sendContact = function () {
+			console.log(this.contact);
 
-        this.sendContact = function () {
-            console.log(this.contact);
-
-            $http({
-                method: 'POST',
-                url: "contact/requestContact.php",
-                data: this.reservation,
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-            }).
-                success(function (data, status, headers, config) {
-                    console.log('contact posted');
-                }).
-                error(function (data, status, headers, config) {
-                    console.log('there was an error posting request  '.data);
-                });
-        };
-    });
+			$http({
+				method : 'POST',
+				url    : "/app/contact/requestContact.php",
+				data   : this.contact,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).
+				success(function (data, status, headers, config) {
+					console.log('contact posted'.data);
+				}).
+				error(function (data, status, headers, config) {
+					console.log(data);
+				});
+		};
+	});
 })();
